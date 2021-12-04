17d6a97d0629679c8fcfe71f48798288fcc238f5
MiltonDevSis <milton.dev.sis@gmail.com>
2021-12-04 17:15:11 -0300

    Implement Detekt in project

--- app/src/main/java/com/mpfcoding/app_marvel/presentation/MainActivity.kt
+++ app/src/main/java/com/mpfcoding/app_marvel/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mpfcoding.app_marvel
+package com.mpfcoding.app_marvel.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mpfcoding.app_marvel.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
