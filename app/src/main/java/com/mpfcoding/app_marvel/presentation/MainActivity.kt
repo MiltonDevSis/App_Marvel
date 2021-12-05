50d7b38daee10379caa310a0e53715f7e8d02e8a
MiltonDevSis <milton.dev.sis@gmail.com>
2021-12-05 12:15:10 -0300

    Implemented Navigation

--- app/src/main/java/com/mpfcoding/app_marvel/presentation/MainActivity.kt
+++ app/src/main/java/com/mpfcoding/app_marvel/presentation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.mpfcoding.app_marvel.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.mpfcoding.app_marvel.R
+import com.mpfcoding.app_marvel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
