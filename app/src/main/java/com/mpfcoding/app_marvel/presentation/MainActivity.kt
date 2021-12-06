0c416a624a7b07538c61048ee8dbedd37190800f
MiltonDevSis <milton.dev.sis@gmail.com>
2021-12-06 19:51:47 -0300

    Implemented and config toolbar

--- app/src/main/java/com/mpfcoding/app_marvel/presentation/MainActivity.kt
+++ app/src/main/java/com/mpfcoding/app_marvel/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mpfcoding.app_marvel.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -19,14 +20,28 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
 
         navController = navHostFragment.navController
 
         binding.bottonNavMain.setupWithNavController(navController)
 
         appBarConfiguration = AppBarConfiguration(
-            setOf()
+            setOf(  // <- declaração dos fragments, top level que não vão aparecer a seta de back button.
+                R.id.charactersFragment,
+                R.id.favoritesFragment,
+                R.id.aboutFragment
+            )
         )
+
+        binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isTopLevelDestination = appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if (!isTopLevelDestination){
+                binding.toolbarApp.setNavigationIcon(R.drawable.ic_back)
+            }
+        }
     }
 }
