f4b327b255acc89ed8032d63ead04af4462559a8
Milton <milton.dev.sis@gmail.com>
2022-01-15 17:00:21 -0300

    Create module DI for repository and remote dataSource

---
+++ app/src/main/java/com/mpfcoding/app_marvel/framework/di/RepositoryModel.kt
@@ -0,0 +1,22 @@
+package com.mpfcoding.app_marvel.framework.di
+
+import com.mpfcoding.app_marvel.framework.CharactersRepositoryImpl
+import com.mpfcoding.app_marvel.framework.network.remote.RetrofitCharactersDataSource
+import com.mpfcoding.app_marvel.framework.network.response.DataWrapperResponse
+import com.mpfcoding.core.data.repository.CharactersRemoteDataSource
+import com.mpfcoding.core.data.repository.CharactersRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModel {
+
+    @Binds
+    fun bindCharactersRepository(repository: CharactersRepositoryImpl): CharactersRepository
+
+    @Binds
+    fun bindRemoteDataSource(datasource: RetrofitCharactersDataSource): CharactersRemoteDataSource<DataWrapperResponse>
+}
