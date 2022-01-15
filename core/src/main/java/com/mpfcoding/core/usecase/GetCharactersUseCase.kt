45d9ca85c6b77a0a9c698fa5fe64d671d27bbf8b
Milton <milton.dev.sis@gmail.com>
2022-01-15 18:12:17 -0300

    Implemented use case for search characters

---
+++ core/src/main/java/com/mpfcoding/core/usecase/GetCharactersUseCase.kt
@@ -0,0 +1,22 @@
+package com.mpfcoding.core.usecase
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.mpfcoding.core.data.repository.CharactersRepository
+import com.mpfcoding.core.domain.model.Character
+import com.mpfcoding.core.usecase.base.PagingUseCase
+import kotlinx.coroutines.flow.Flow
+
+class GetCharactersUseCase(
+    private val charactersRepository: CharactersRepository
+) : PagingUseCase<GetCharactersUseCase.GetCharactersParams, Character>() {
+
+    override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
+        return Pager(config = params.config){
+            charactersRepository.getCharacters(params.query)
+        }.flow
+    }
+
+    data class GetCharactersParams(val query: String, val config: PagingConfig)
+}
