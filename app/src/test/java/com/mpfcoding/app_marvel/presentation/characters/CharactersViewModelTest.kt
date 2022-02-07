3cdac666e57c237d37a1f1d0dd9063e2503f7976
Milton <milton.dev.sis@gmail.com>
2022-02-06 22:24:00 -0300

    Implement unit test for view model characters part_01

---
+++ app/src/test/java/com/mpfcoding/app_marvel/presentation/characters/CharactersViewModelTest.kt
@@ -0,0 +1,77 @@
+package com.mpfcoding.app_marvel.presentation.characters
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.mpfcoding.core.domain.model.Character
+import com.mpfcoding.core.usecase.GetCharactersUseCase
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class CharactersViewModelTest {
+
+    @ExperimentalCoroutinesApi
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    @Mock
+    lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    private lateinit var charactersViewModel: CharactersViewModel
+
+    private val pagingDataCharacters = PagingData.from(
+        listOf(
+            Character(
+                name = "3-D Man",
+                imageUrl = "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+            ),
+            Character(
+                name = "A-Bomb (HAS)",
+                imageUrl = "https://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg"
+            )
+        )
+    )
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+        charactersViewModel = CharactersViewModel(getCharactersUseCase)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should validate the paging data object values when calling CharactersPagingData`() =
+        runBlockingTest {
+
+            whenever(
+                getCharactersUseCase.invoke(any())
+            ).thenReturn(
+                flowOf(pagingDataCharacters)
+            )
+
+            val expectedPaginData = pagingDataCharacters
+
+            val result = charactersViewModel.charactersPagingData("").first()
+
+            result.map { resultCharacter ->
+                expectedPaginData.map {
+                   assertEquals(it.name, resultCharacter.name)
+                }
+            }
+        }
+}
