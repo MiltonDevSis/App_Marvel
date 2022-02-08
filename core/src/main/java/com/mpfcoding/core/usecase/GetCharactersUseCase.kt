a01c82cb608c119ec98216d5c77a6a52eff3cea0
Milton <milton.dev.sis@gmail.com>
2022-02-07 22:04:19 -0300

    Refactor GetCharacterUseCase for adapt UnitTest

--- core/src/main/java/com/mpfcoding/core/usecase/GetCharactersUseCase.kt
+++ core/src/main/java/com/mpfcoding/core/usecase/GetCharactersUseCase.kt
@@ -5,20 +5,25 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.mpfcoding.core.data.repository.CharactersRepository
 import com.mpfcoding.core.domain.model.Character
+import com.mpfcoding.core.usecase.GetCharactersUseCase.GetCharactersParams
 import com.mpfcoding.core.usecase.base.PagingUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCharactersUseCase
-@Inject constructor(
+interface GetCharactersUseCase {
+    operator fun invoke(params: GetCharactersParams): Flow<PagingData<Character>>
+
+    data class GetCharactersParams(val query: String, val config: PagingConfig)
+}
+
+class GetCharactersUseCaseImpl @Inject constructor(
     private val charactersRepository: CharactersRepository
-) : PagingUseCase<GetCharactersUseCase.GetCharactersParams, Character>() {
+) : PagingUseCase<GetCharactersParams, Character>(),
+    GetCharactersUseCase {
 
     override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
-        return Pager(config = params.config){
+        return Pager(config = params.config) {
             charactersRepository.getCharacters(params.query)
         }.flow
     }
-
-    data class GetCharactersParams(val query: String, val config: PagingConfig)
 }
