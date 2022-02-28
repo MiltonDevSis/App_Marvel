528d1de23ba9abbc0341f24a06570a7556525cd2
Milton <milton.dev.sis@gmail.com>
2022-02-28 17:50:42 -0300

    Finished test and implemented pattern AAA and factory to DataWrapper

--- app/src/test/java/com/mpfcoding/app_marvel/framework/network/paging/CharactersPagingSourceTest.kt
+++ app/src/test/java/com/mpfcoding/app_marvel/framework/network/paging/CharactersPagingSourceTest.kt
@@ -3,13 +3,20 @@ package com.mpfcoding.app_marvel.framework.network.paging
 import androidx.paging.PagingSource
 import com.mpfcoding.app_marvel.framework.network.response.DataWrapperResponse
 import com.mpfcoding.core.data.repository.CharactersRemoteDataSource
+import com.mpfcoding.core.domain.model.Character
+import com.mpfcoding.factory.response.DataWrapperResponseFactory
 import com.mpfcoding.testing.MainCoroutineRule
+import com.mpfcoding.testing.model.CharacterFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -19,9 +26,14 @@ class CharactersPagingSourceTest{
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
-    private lateinit var charactersPagingSource: CharactersPagingSource
+    @Mock
+    lateinit var remoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
 
-    private lateinit var remoteDataSource: CharactersRemoteDataSource<DataWrapperResponse>
+    private val dataWrapperResponseFactory = DataWrapperResponseFactory()
+
+    private val characterFactory = CharacterFactory()
+
+    private lateinit var charactersPagingSource: CharactersPagingSource
 
     @Before
     fun setup(){
@@ -31,13 +43,32 @@ class CharactersPagingSourceTest{
     @ExperimentalCoroutinesApi
     @Test
     fun `should return a sucess load result when load is called`() = runBlockingTest{
+        //arrange
+        whenever(remoteDataSource.fetchCharacters(any()))
+            .thenReturn(dataWrapperResponseFactory.create())
 
+        // act
         val result = charactersPagingSource.load(
             params = PagingSource.LoadParams.Refresh(
                 key = null,
                 loadSize = 2,
                 placeholdersEnabled = false
             )
+        )
+
+        // assert
+        val expected = listOf(
+            characterFactory.create(CharacterFactory.Hero.TreeDMan),
+            characterFactory.create(CharacterFactory.Hero.Abomb)
+        )
+
+        assertEquals(
+            PagingSource.LoadResult.Page(
+                data = expected,
+                prevKey = null,
+                nextKey = 20
+            ),
+            result
         )
     }
 
