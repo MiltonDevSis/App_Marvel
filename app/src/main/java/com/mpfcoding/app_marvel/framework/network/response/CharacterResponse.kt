d01dd70a92978b2b4a2d3031b2273213c6c52966
Milton <milton.dev.sis@gmail.com>
2022-03-01 18:21:04 -0300

    Making the app more secure by configuring proguard

--- app/src/main/java/com/mpfcoding/app_marvel/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/mpfcoding/app_marvel/framework/network/response/CharacterResponse.kt
@@ -1,10 +1,14 @@
 package com.mpfcoding.app_marvel.framework.network.response
 
+import com.google.gson.annotations.SerializedName
 import com.mpfcoding.core.domain.model.Character
 
 data class CharacterResponse(
+    @SerializedName("id")
     val id: String,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("thumbnail")
     val thumbnail: ThumbnailResponse
 )
 
--- app/src/main/java/com/mpfcoding/app_marvel/framework/network/response/DataWrapperResponse.kt
+++ app/src/main/java/com/mpfcoding/app_marvel/framework/network/response/DataWrapperResponse.kt
@@ -1,6 +1,10 @@
 package com.mpfcoding.app_marvel.framework.network.response
 
+import com.google.gson.annotations.SerializedName
+
 data class DataWrapperResponse(
+    @SerializedName("copyright")
     val copyright: String,
+    @SerializedName("data")
     val data: DataContainerResponse
 )
