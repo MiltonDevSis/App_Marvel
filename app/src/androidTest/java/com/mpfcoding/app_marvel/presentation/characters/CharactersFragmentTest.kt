593ec3f352fb45afbf44e10b203028e213775282
Milton <milton.dev.sis@gmail.com>
2022-03-17 19:27:45 -0300

    Config hilt for test with espresso

--- app/src/androidTest/java/com/mpfcoding/app_marvel/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/mpfcoding/app_marvel/presentation/characters/CharactersFragmentTest.kt
@@ -2,12 +2,19 @@ package com.mpfcoding.app_marvel.presentation.characters
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class CharactersFragmentTest{
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Before
     fun setup(){
