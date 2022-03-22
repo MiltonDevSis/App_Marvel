4c4ecb6aaf4a2e810fd225c88248fadc9121ad2f
Milton <milton.dev.sis@gmail.com>
2022-03-22 15:19:39 -0300

    Implemented first test instrumented

--- app/src/androidTest/java/com/mpfcoding/app_marvel/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/mpfcoding/app_marvel/presentation/characters/CharactersFragmentTest.kt
@@ -1,9 +1,17 @@
 package com.mpfcoding.app_marvel.presentation.characters
 
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.mpfcoding.app_marvel.R
 import com.mpfcoding.app_marvel.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.Matcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,6 +31,7 @@ class CharactersFragmentTest{
 
     @Test
     fun shouldShowCharactersWhenViewIsCreated(){
-
+        Thread.sleep(3000)
+        onView(withId(R.id.recycler_character)).check(matches(isDisplayed()))
     }
 }
