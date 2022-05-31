3689500eff8f4a7a432ca0ad369ed7f815e56961
Milton <milton.dev.sis@gmail.com>
2022-05-31 20:32:57 -0300

    Update coroutines version to 1.6.0

--- app/src/test/java/com/mpfcoding/app_marvel/framework/paging/CharactersPagingSourceTest.kt
+++ app/src/test/java/com/mpfcoding/app_marvel/framework/paging/CharactersPagingSourceTest.kt
@@ -9,7 +9,7 @@ import com.mpfcoding.testing.model.CharacterFactory
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -40,7 +40,7 @@ class CharactersPagingSourceTest {
     }
 
     @Test
-    fun `should return a sucess load result when load is called`() = runBlockingTest {
+    fun `should return a sucess load result when load is called`() = runTest {
         //arrange
         whenever(remoteDataSource.fetchCharacters(any()))
             .thenReturn(dataWrapperResponseFactory.create())
@@ -71,7 +71,7 @@ class CharactersPagingSourceTest {
     }
 
     @Test
-    fun `should return a error load result when load is called`() = runBlockingTest {
+    fun `should return a error load result when load is called`() = runTest {
         // arrange
         val exception = RuntimeException()
         whenever(remoteDataSource.fetchCharacters(any()))
