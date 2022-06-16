223b0d27396e58436923ea0651bf74fb1e98e4af
Milton <milton.dev.sis@gmail.com>
2022-06-16 15:28:46 -0300

    Create Room App Database

---
+++ app/src/main/java/com/mpfcoding/app_marvel/framework/di/DatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.mpfcoding.app_marvel.framework.di
+
+import android.content.Context
+import androidx.room.Room
+import com.mpfcoding.app_marvel.framework.db.AppDatabase
+import com.mpfcoding.core.data.DbConstants.APP_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    fun provideAppDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        AppDatabase::class.java,
+        APP_DATABASE_NAME
+    ).build()
+}
