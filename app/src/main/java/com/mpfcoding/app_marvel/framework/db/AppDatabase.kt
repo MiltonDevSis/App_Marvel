db7d88901b887dbb8f5564d7f6a84edf212b65d9
Milton <milton.dev.sis@gmail.com>
2022-07-17 19:59:32 -0300

    Implemented Entity and Dao to remote keys

--- app/src/main/java/com/mpfcoding/app_marvel/framework/db/AppDatabase.kt
+++ app/src/main/java/com/mpfcoding/app_marvel/framework/db/AppDatabase.kt
@@ -2,11 +2,27 @@ package com.mpfcoding.app_marvel.framework.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.mpfcoding.app_marvel.framework.db.dao.CharacterDao
 import com.mpfcoding.app_marvel.framework.db.dao.FavoriteDao
+import com.mpfcoding.app_marvel.framework.db.dao.RemoteKeyDao
+import com.mpfcoding.app_marvel.framework.db.entity.CharacterEntity
 import com.mpfcoding.app_marvel.framework.db.entity.FavoriteEntity
+import com.mpfcoding.app_marvel.framework.db.entity.RemoteKey
 
-@Database(entities = [FavoriteEntity::class], version = 1, exportSchema = false)
-abstract class AppDatabase: RoomDatabase() {
+@Database(
+    entities = [
+        FavoriteEntity::class,
+        CharacterEntity::class,
+        RemoteKey::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun favoriteDao(): FavoriteDao
+
+    abstract fun characterDao(): CharacterDao
+
+    abstract fun remoteKeyDao(): RemoteKeyDao
 }
