0c57d491ef0f59b903060632176c736921fa0495
Milton <milton.dev.sis@gmail.com>
2022-08-06 16:12:36 -0300

    Create new feature for ordination

---
+++ app/src/main/java/com/mpfcoding/app_marvel/presentation/sort/SortFragment.kt
@@ -0,0 +1,36 @@
+package com.mpfcoding.app_marvel.presentation.sort
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.mpfcoding.app_marvel.R
+import com.mpfcoding.app_marvel.databinding.FragmentSortBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SortFragment : Fragment() {
+
+    private var _binding: FragmentSortBinding? = null
+    private val binding: FragmentSortBinding get() = _binding!!
+
+    private val viewModel: SortViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentSortBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/mpfcoding/app_marvel/presentation/sort/SortViewModel.kt
@@ -0,0 +1,10 @@
+package com.mpfcoding.app_marvel.presentation.sort
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SortViewModel @Inject constructor() : ViewModel() {
+
+}
